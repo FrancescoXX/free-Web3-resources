9110a2f1290fe6f50acc3ccd18d40e6aa6400b8f
Ansh <99464146+AnshMinchekar@users.noreply.github.com>
2022-10-09 12:44:53 +0530

    Update ERC20Token.sol

--- code-snippets/ERC20Token.sol
+++ code-snippets/ERC20Token.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.0;
 //Import ERC20 Contract from OpenZepplin
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 contract TokenName is ERC20 {
-    //Need to call constructor present in ERC20
+    //Need to call constructor present in ERC20 contract
     //constructor(string memory _name, string memory _symbol) ERC20(_name,_symbol)
     constructor() ERC20("TOKEN_NAME","TKN") {
         //msg.sender is the person deploying the contract
