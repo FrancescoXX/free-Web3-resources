159a6ecbded2b668aa05dcca4652f29705799c95
othaime-en <61498231+othaime-en@users.noreply.github.com>
2022-11-10 08:36:38 +0000

    Refactor the code for easy readability

--- code-snippets/functsolidity.sol
+++ code-snippets/functsolidity.sol
@@ -1,28 +1,25 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity >=0.5.0 < 0.9.0;
-// program to return the value of function 
+
+// A program to return the value of variables using a function 
 contract nameAndAge {
     string name;
     uint age;
     
-    constructor() public{
-    
-    
-    name='Gitansh';
-    age=21;
-    
-
-    function getName() view public returns(string memory){
-    // function will return age
-    return name;
-
-    
+    constructor() {
+        name='Gitansh';
+        age=21;
     }
-    function getName() view public returns(uint ){
-    //function will return age
-    return age;
-    
-    
+
+    // This function returns the name
+    function getName() public view returns(string memory){
+        return name;
+    }
+        
+    // This function returns the age
+    function getAge() public view returns(uint ){
+        return age;
     }
     
-
 }
