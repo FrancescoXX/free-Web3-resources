5a2c5f704300f10d1643902a3a6fefefb21e51e6
Francesco Ciulla <francescociullaroma@gmail.com>
2022-12-28 12:54:09 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/FrancescoXX/free-Web3-resources',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/FrancescoXX/free-Web3-resources',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
