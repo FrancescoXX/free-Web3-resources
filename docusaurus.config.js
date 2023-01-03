d2911c750067a410fb2b3eb1f701718657374aa2
tobySolutions <adedejitobiloba7@gmail.com>
2023-01-03 01:15:19 +0100

    start cleaning directory

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,53 +1,53 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Free Web3 resources",
-  tagline: "A list of resources to learn Web3",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  title: 'Free Web3 resources',
+  tagline: 'A list of resources to learn Web3',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: '/static/img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/FrancescoXX/free-Web3-resources/edit/main",
+            'https://github.com/FrancescoXX/free-Web3-resources/edit/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/FrancescoXX/free-Web3-resources/edit/main",
+            'https://github.com/FrancescoXX/free-Web3-resources/edit/main',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
@@ -57,56 +57,56 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "Free Web3 Resources",
+        title: 'Free Web3 Resources',
         logo: {
-          alt: "Free Web3 resources",
-          src: "img/logo.png",
+          alt: 'Free Web3 resources',
+          src: 'img/logo.png',
         },
         items: [
           {
-            type: "doc",
-            docId: "intro",
-            position: "left",
-            label: "Resources",
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Resources',
           },
           {
-            href: "https://github.com/FrancescoXX/free-Web3-resources",
-            label: "GitHub",
-            position: "right",
+            href: 'https://github.com/FrancescoXX/free-Web3-resources',
+            label: 'GitHub',
+            position: 'right',
           },
         ],
       },
       footer: {
-        style: "dark",
+        style: 'dark',
         links: [
           {
-            title: "Docs",
+            title: 'Docs',
             items: [
               {
-                label: "Tutorial",
-                to: "/docs/intro",
+                label: 'Tutorial',
+                to: '/docs/intro',
               },
             ],
           },
           {
-            title: "Community",
+            title: 'Community',
             items: [
               {
-                label: "Discord",
-                href: "https://discord.com/invite/JZ2btyTfzQ",
+                label: 'Discord',
+                href: 'https://discord.com/invite/JZ2btyTfzQ',
               },
               {
-                label: "Twitter",
-                href: "https://twitter.com/4ccommunityhq",
+                label: 'Twitter',
+                href: 'https://twitter.com/4ccommunityhq',
               },
             ],
           },
           {
-            title: "More",
+            title: 'More',
             items: [
               {
-                label: "GitHub",
-                href: "https://github.com/FrancescoXX/free-Web3-resources",
+                label: 'GitHub',
+                href: 'https://github.com/FrancescoXX/free-Web3-resources',
               },
             ],
           },
