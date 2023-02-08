346b0a6c20ffba282c3037dd8cc46be266911ccf
lambeboluwatife <boluwatifelambe@gmail.com>
2023-02-08 12:23:57 +0100

    fix: mobile break view

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,46 +1,43 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Easy to Use",
+    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
-      <>
-        This is a simple and easy to use website to find resources for web3
-      </>
+      <>This is a simple and easy to use website to find resources for web3</>
     ),
   },
   {
-    title: 'Managed by a community',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Managed by a community",
+    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
-      <>
-        The 4c community is the community behind the free web3 resources site
-      </>
+      <>The 4c community is the community behind the free web3 resources site</>
     ),
   },
   {
-    title: 'Powered by Docusaurus',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Powered by Docusaurus",
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
-        The React Docusaurus framework for making documentation is what this site was built with.
+        The React Docusaurus framework for making documentation is what this
+        site was built with.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       {/* <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div> */}
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
-        <p>{description}</p>
+        <p style={{ marginBottom: "10px" }}>{description}</p>
       </div>
     </div>
   );
