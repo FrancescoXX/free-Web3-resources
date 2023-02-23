e234e797401e825ea7804156463e5e4e0d4be594
Rohinish <singhrohinish09@gmail.com>
2023-02-23 21:17:47 +0530

    fix: changed inconsistent spacing

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -38,7 +38,7 @@ function Feature({Svg, title, description}) {
       {/* <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div> */}
-      <div className="text--center padding-horiz--md">
+      <div className="text--center padding-horiz--md ">
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
