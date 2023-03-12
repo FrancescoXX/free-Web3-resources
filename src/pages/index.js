be68be36fa99af9f14c5f90b0739521841b10f34
DevvSakib <devvsakib@gmail.com>
2023-03-12 12:45:11 +0600

    removed title

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,6 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
